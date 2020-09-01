cf9f36d4bab922259dfd6421a7deb9ca7620b37f
glitznerf <glitznerf@gmail.com>
2020-09-01 13:20:33 +0100

    Bias API requests implemented

--- execution_backend.py
+++ execution_backend.py
@@ -10,6 +10,7 @@ class Broker:
         self.platform = platform
         self.api_url = api_url
 
+    # Place order, return success as bool
     def order(self, symbol, amount, price):
         # Determine whether buy or sell order
         if amount > 0:
@@ -17,7 +18,7 @@ class Broker:
         elif amount < 0:
             side = "SELL"
         else:
-            assert True=False
+            return False
 
         # Execute order
         try:
@@ -62,16 +63,33 @@ class Broker:
                 prices.append(response.json()["price"])
             except HTTPError as http_err:
                 print("HTTPError: ", http_err)
-                assert True==False
+                return False
             except Exception as err:
                 print("Error: ", err)
-                assert True==False
+                return False
         return prices
 
-    def get_volatility(self, *args):
-        # Binance API get volatility for *args
-        vol = ""
-        return vol
+    def get_bias(self, *args):
+        quantity, bias = [], []
+        for arg in args:
+            try:
+                response = requests.get(self.api_url + (f"depth?symbol={arg}"))
+                response.raise_for_status()
+                quant_bid = 0.0
+                quant_ask = 0.0
+                for bid in response.json()["bids"]:
+                    quant_bid += float(bid[1])
+                for ask in response.json()["asks"]:
+                    quant_ask += float(ask[1])
+                quantity.append(quant_bid+quant_ask)
+                bias.append(quant_bid-quant_ask)
+            except HTTPError as http_err:
+                print("HTTPError: ", http_err)
+                return False
+            except Exception as err:
+                print("Error: ", err)
+                return False
+        return quantity, bias
 
     def get_fees(self, *args):
         # Binance API get fees for *args

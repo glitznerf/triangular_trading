dd76a2b626c4eba3644d861ab71433398feeeeee
glitznerf <glitznerf@gmail.com>
2020-12-09 18:23:46 +0000

    Basic class information

---
+++ Broker.py
@@ -0,0 +1,11 @@
+# This Broker is supposed to connect to the Binance RUST API and get current market information
+# All Binance requests return either a JSON object or an array
+# The Binance API Docs are available here: https://binance-docs.github.io/apidocs
+
+class Broker:
+    def __init__(self):
+        pass
+
+    # Get current average price for symbol
+
+    # Get orderbook information for symbol

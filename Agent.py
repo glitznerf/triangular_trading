753cff8fe940e7a38901d99301155baacc93cc2b
glitznerf <glitznerf@gmail.com>
2020-12-09 23:27:33 +0000

    Incorporated Broker changes, New Status print

--- Agent.py
+++ Agent.py
@@ -12,31 +12,35 @@ class Agent:
 
     # Print status of the Agent
     def status(self):
-        print("Base currency", self.base)
-        print("Broker OK") if self.broker.ping() else print("Broker not connected")
-        print("Pairs OK") if len(self.pairs) != 0 else print("Pairs not set up")
-        print("Triangles OK") if len(self.triangles) != 0 else print("Triangles not set up")
+        print("Agent Status:")
+        print("  Base currency:", self.base)
+        print("  Broker: OK") if self.broker.ping() else print("  Broker: Not connected")
+        print("  Pairs: OK") if len(self.pairs) != 0 else print("  Pairs: Not set up")
+        print("  Triangles: OK (" + str(len(self.triangles)) + ")") if len(self.triangles) != 0 else print("  Triangles: Not set up")
+        print("")
 
 
     # Get available currency pairs from broker and set up triangle structures
     # We set up base_currency -> b -> c -> base_currency
     # Returns: triangles (array of 3-tuples)
     def setup_pairs(self):
-        symbols = self.broker.exchange_info()["symbols"]
-        for symbol in symbols:
-            if symbol["status"] == "TRADING":
-                pair = symbol["baseAsset"], symbol["quoteAsset"]
-                self.pairs.append(pair)
-        for one in self.pairs:
-            if self.base in one:
-                b = one[0] if one[0] != self.base else one[1]
-                for two in self.pairs:
-                    if b in two and not self.base in two:
-                        c = two[0] if two[0] != b else two[1]
-                        for three in self.pairs:
-                            if (self.base in three) and (c in three):
-                                triangle = one,two,three
-                                self.triangles.append(triangle)
+        exchange_info = self.broker.exchange_info()
+        if exchange_info[0]:
+            symbols = exchange_info[1]["symbols"]
+            for symbol in symbols:
+                if symbol["status"] == "TRADING":
+                    pair = symbol["baseAsset"], symbol["quoteAsset"]
+                    self.pairs.append(pair)
+            for one in self.pairs:
+                if self.base in one:
+                    b = one[0] if one[0] != self.base else one[1]
+                    for two in self.pairs:
+                        if b in two and not self.base in two:
+                            c = two[0] if two[0] != b else two[1]
+                            for three in self.pairs:
+                                if (self.base in three) and (c in three):
+                                    triangle = one,two,three
+                                    self.triangles.append(triangle)
 
 
 
@@ -45,5 +49,4 @@ B = Broker()
 A = Agent(B)
 A.status()
 A.setup_pairs()
-print()
 A.status()

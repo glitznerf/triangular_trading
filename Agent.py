9101ddffc881d370f94f7dbae9212e5f03e2aec4
glitznerf <glitznerf@gmail.com>
2020-12-10 00:03:00 +0000

    Calculate effective price, spot arbitrage opportunities

--- Agent.py
+++ Agent.py
@@ -33,14 +33,42 @@ class Agent:
                     self.pairs.append(pair)
             for one in self.pairs:
                 if self.base in one:
-                    b = one[0] if one[0] != self.base else one[1]
+                    b = (one[0] if one[0] != self.base else one[1])
                     for two in self.pairs:
                         if b in two and not self.base in two:
-                            c = two[0] if two[0] != b else two[1]
+                            c = (two[0] if two[0] != b else two[1])
                             for three in self.pairs:
                                 if (self.base in three) and (c in three):
-                                    triangle = one,two,three
-                                    self.triangles.append(triangle)
+                                    triangle = one, two, three
+                                    self.triangles.append(((b,c),triangle))
+
+
+    # Calculate effective triangular price
+    # ToDo: incorporate fees
+    # ToDo: incorporate false broker response
+    # Returns: effective price (float)
+    def eff_price(self, triangle):
+        pairs = ["".join(pair) for pair in triangle[1]]
+        prices = [float(self.broker.get_price(pair)[1]["price"]) for pair in pairs]
+        directions = []
+        for ix, curr in enumerate([self.base, triangle[0][0], triangle[0][1]]):
+            directions.append(True if curr == triangle[1][ix][1] else False)
+        effective = 1
+        for ix, price in enumerate(prices):
+            if directions[ix]:
+                effective = effective *price
+            else:
+                effective = effective / price
+        return effective
+
+
+    # ToDo: iterate over triangles calculating effective price until arbitrage opportunity is given
+    def opportunity(self):
+        assert len(self.triangles) > 0
+        for triangle in self.triangles:
+            effective = self.eff_price(triangle)
+            if effective > 1:
+                print(effective, triangle)
 
 
 
@@ -50,3 +78,4 @@ A = Agent(B)
 A.status()
 A.setup_pairs()
 A.status()
+A.opportunity()

efe202a258d0e2d95ad160abc3ba95b99479fb06
glitznerf <glitznerf@gmail.com>
2020-12-22 11:50:59 +0100

    A simple, useless arbitrage ticker

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # The main execution of the program
 
-import Broker
-import Agent
+from Broker import Broker
+from Agent import Agent
+import time
 
 
 # Init and set up Broker and Agent
 B = Broker()
+
 A = Agent(B)
 A.setup_pairs()
 A.status()
@@ -13,5 +15,5 @@ A.status()
 
 # A simple opportunity ticker
 while True:
-    print(A.opportunities()[0])
+    print(A.opportunities()[1][0][0])
     time.sleep(1)
